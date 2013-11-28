99c1acb60fd4ca7ac6733e9c83b99a963153c81b
Neil Jin <zhao.jin@liferay.com>
2013-11-28 18:53:19 +0800

    format source. changed q to question. changed the condition of check the protrait

--- who-is-quiz-portlet/src/main/java/com/liferay/projects/quiz/QuizPortlet.java
+++ who-is-quiz-portlet/src/main/java/com/liferay/projects/quiz/QuizPortlet.java
@@ -28,25 +28,30 @@ import com.liferay.util.bridges.mvc.MVCPortlet;
  */
 public class QuizPortlet extends MVCPortlet {
 
-	public void startQuiz( ActionRequest request, ActionResponse response )
+	public void startQuiz( ActionRequest request, ActionResponse response)
 			throws PortalException, SystemException {
 //		final String quizFilter = ParamUtil.getString( request, "quizFilter" );
-		int numQuestions = ParamUtil.getInteger( request, "numQuestions" );
+		int numQuestions = ParamUtil.getInteger(request, "numQuestions");
 
-		ThemeDisplay themeDisplay = (ThemeDisplay) request.getAttribute( WebKeys.THEME_DISPLAY );
+		ThemeDisplay themeDisplay = (ThemeDisplay) request.getAttribute(
+				WebKeys.THEME_DISPLAY);
 		long companyId = themeDisplay.getCompanyId();
 
-		Organization dalianOrg = OrganizationLocalServiceUtil.getOrganization( companyId, "Liferay Dalian" );
+		Organization dalianOrg = OrganizationLocalServiceUtil.getOrganization(
+				companyId, "Liferay Dalian");
 
-		List<User> orgUsers = UserLocalServiceUtil.getOrganizationUsers( dalianOrg.getOrganizationId() );
+		List<User> orgUsers = UserLocalServiceUtil.getOrganizationUsers(
+				dalianOrg.getOrganizationId());
 
-		List<User> pictureUsers = removeUsersWithoutProfilePicture( orgUsers, themeDisplay );
+		List<User> pictureUsers = removeUsersWithoutProfilePicture(
+				orgUsers, themeDisplay);
 
-		List<User> users = Collections.unmodifiableList( pictureUsers );
+		List<User> users = Collections.unmodifiableList(pictureUsers);
 
-		List<User> quizUsers = pickAtRandom( users, numQuestions );
+		List<User> quizUsers = pickAtRandom(users, numQuestions);
 
-		Question[] questions = generateQuestions(quizUsers, users, numQuestions);
+		Question[] questions = generateQuestions(
+				quizUsers, users, numQuestions);
 
 		User[] answers = new User[questions.length];
 
@@ -54,9 +59,9 @@ public class QuizPortlet extends MVCPortlet {
 		portletSession.setAttribute("questions", questions);
 		portletSession.setAttribute("answers", answers);
 
-		response.setRenderParameter( "numQuestions", StringUtil.valueOf(numQuestions) );
-		response.setRenderParameter( "questionIndex", "0" );
-		response.setRenderParameter( "jspPage", "/html/question.jsp" );
+		response.setRenderParameter("numQuestions", StringUtil.valueOf(numQuestions));
+		response.setRenderParameter("questionIndex", "0");
+		response.setRenderParameter("jspPage", "/html/question.jsp");
 
 	}
 
@@ -74,102 +79,89 @@ public class QuizPortlet extends MVCPortlet {
 
 		long choiceUserId = ParamUtil.getLong(request, "choice");
 
-		final User choiceUser = UserLocalServiceUtil.getUser( choiceUserId );
+		final User choiceUser = UserLocalServiceUtil.getUser(choiceUserId);
 		answers[questionIndex] = choiceUser;
 
 		// last page? 
-		if( ( questions.length - 1 ) == questionIndex ) {
-			response.setRenderParameter( "jspPage", "/html/results.jsp");
+		if ((questions.length - 1) == questionIndex) {
+			response.setRenderParameter("jspPage", "/html/results.jsp");
 		}
 		else {
-			response.setRenderParameter( "numQuestions", numQuestions);
-			response.setRenderParameter( "questionIndex", ( questionIndex + 1 ) + "");
+			response.setRenderParameter("numQuestions", numQuestions);
+			response.setRenderParameter("questionIndex", StringUtil.valueOf(questionIndex + 1));
 			response.setRenderParameter( "jspPage", "/html/question.jsp");
 		}
 	}
 
-	private Question[] generateQuestions( List<User> quizUsers, List<User> poolUsers, int numQuestions )
-	{
+	private Question[] generateQuestions(
+			List<User> quizUsers, List<User> poolUsers, int numQuestions )
+					throws PortalException, SystemException {
+
 		List<Question> retval = new ArrayList<Question>();
 
-		final int poolSize = poolUsers.size();
+		int poolSize = poolUsers.size();
 
-		for( User user : quizUsers )
-		{
-			Question q = new Question( user );
+		for (User user : quizUsers) {
+			Question question = new Question(user);
 
 			// find 3 other users in the pool of users that are the same gender of question user
-			for( int i = 0; i < 3; i++ )
-			{
-				User choice = poolUsers.get( _rand.nextInt( poolSize ) );
+			for (int i = 0; i < 3; i++) {
+				User choice = poolUsers.get(_rand.nextInt(poolSize));
 
-				while( choice == user || ( !isSameGender( choice, user ) ) || q.getChoices().contains( choice ) )
-				{
-					choice = poolUsers.get( _rand.nextInt( poolSize ) );
+				while (
+						choice == user 
+						|| (!isSameGender(choice, user))
+						|| question.getChoices().contains(choice)) {
+
+					choice = poolUsers.get(_rand.nextInt(poolSize));
 				}
 
-				q.addChoice( choice );
+				question.addChoice(choice);
 			}
 
-			retval.add( q );
+			retval.add(question);
 		}
 
-		return retval.toArray( new Question[0] );
+		return retval.toArray(new Question[0]);
 	}
 
-	private boolean isSameGender( User choice, User user )
+	private boolean isSameGender(User choice, User user)
+			throws PortalException, SystemException
 	{
-		try
-		{
-			return choice.getMale() == user.getMale();
-		}
-		catch( Exception e )
-		{
-		}
-		return false;
+		return choice.getMale() == user.getMale();
 	}
 
-	private List<User> pickAtRandom( List<User> users, int numOfPicks )
-	{
-		final List<User> retval = new ArrayList<User>();
+	private List<User> pickAtRandom(List<User> users, int numOfPicks) {
+		List<User> retval = new ArrayList<User>();
 
-		final int size = users.size();
+		int size = users.size();
 
-		for( int i = 0; i < numOfPicks; i++ )
-		{
-			User nextUser = users.get( _rand.nextInt( size ) );
+		for (int i = 0; i < numOfPicks; i++) {
+			User nextUser = users.get(_rand.nextInt( size ));
 
 			//while( retval.contains( nextUser ) )
 			//{
 				//nextUser = users.get( _rand.nextInt( size ) );
 			//}
 
-			retval.add( nextUser );
+			retval.add(nextUser);
 		}
 
-		return Collections.unmodifiableList( retval );
+		return Collections.unmodifiableList(retval);
 	}
 
-	private List<User> removeUsersWithoutProfilePicture( List<User> users, ThemeDisplay themeDisplay )
-	{
+	private List<User> removeUsersWithoutProfilePicture(
+			List<User> users, ThemeDisplay themeDisplay ) {
 		List<User> retval = new ArrayList<User>();
 
-		for( User user : users )
-		{
-			try
-			{
-				String portrait = user.getPortraitURL( themeDisplay );
+		for (User user : users) {
+			long portraitId = user.getPortraitId();
 
-				if( portrait.contains( "img_id=0" ) )
-				{
-					continue;
-				}
-			}
-			catch( Exception e )
-			{
+			if (portraitId == 0) {
+				continue;
 			}
 
-			retval.add( user );
+			retval.add(user);
 		}
 
 		return retval;
