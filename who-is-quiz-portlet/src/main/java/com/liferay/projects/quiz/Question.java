baefde28149e0f388eb45fb66239cdaa737f37f6
Neil Jin <zhao.jin@liferay.com>
2013-11-28 18:55:48 +0800

    formated source of Quesiton.java

--- who-is-quiz-portlet/src/main/java/com/liferay/projects/quiz/Question.java
+++ who-is-quiz-portlet/src/main/java/com/liferay/projects/quiz/Question.java
@@ -1,36 +1,34 @@
 package com.liferay.projects.quiz;
 
+import com.liferay.portal.model.User;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
-import com.liferay.portal.model.User;
-
 public class Question {
 
-	private User user;
-	private List<User> choices;
-
 	public Question(User user) {
-		this.user = user;
-		this.choices = new ArrayList<User>();
-		this.choices.add(user);
+		_user = user;
+		_choices = new ArrayList<User>();
+		_choices.add(user);
 	}
 
 	public void addChoice(User choice) {
-		this.choices.add(choice);
+		_choices.add(choice);
 	}
 
 	public User getUser() {
-		return user;
+		return _user;
 	}
 
 	public List<User> getChoices() {
 		long seed = System.nanoTime();
-		Collections.shuffle(this.choices, new Random(seed));
-		
-		return this.choices;
-	}
+		Collections.shuffle(_choices, new Random(seed));
 
+		return _choices;
+	}
+	private User _user;
+	private List<User> _choices;
 }
